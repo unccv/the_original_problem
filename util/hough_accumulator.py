c7fcb74ccd2676062ce8148e60d61754f1b8d7d1
josiahls <josiahls@users.noreply.github.com>
2018-08-24 16:26:01 -0400

    Fixed Accumulator Compilation Errors

--- util/hough_accumulator.py
+++ util/hough_accumulator.py
@@ -8,8 +8,9 @@
 
 import numpy as np
 
+
 class HoughAccumulator(object):
-    def __init__(self, theta_bins, phi_bins, phi_min, phi_max):
+    def __init__(self, theta_bins: int, phi_bins: int, phi_min: int, phi_max: int):
         '''
         Simple class to implement an accumalator for the hough transform. 
         Args: 
@@ -18,46 +19,48 @@ class HoughAccumulator(object):
         phi_max = maximum phi value to accumulate
         phi_min = minimu phi value to acumulate
         '''
-        self.accumulator = np.zeros((phi_bins, theta_bins))
+        self.theta_bins = theta_bins
+        self.phi_bins = phi_bins
+        self.accumulator = np.zeros((self.phi_bins, self.theta_bins))
 
-        #This covers all possible lines:
+        # This covers all possible lines:
         theta_min = 0
         theta_max = np.pi
 
-        #Compute the phi and theta values for the grids in our accumulator:
-        self.rhos = np.linspace(rho_min, rho_max, self.accumulator.shape[0])
+        # Compute the phi and theta values for the grids in our accumulator:
+        self.rhos = np.linspace(phi_min, phi_max, self.accumulator.shape[0])
         self.thetas = np.linspace(theta_min, theta_max, self.accumulator.shape[1])
 
-    def accumulate(self, x_coords, y_coords):
+    def accumulate(self, x_coords: list, y_coords: list):
         '''
         Iterate through x and y coordinates, accumulate in hough space, and return. 
         Args: 
         x_coords = x-coordinates of points to transform
-        y_coords = y-coordinats of poits to transform
+        y_coords = y-coordinats of points to transform
 
         Returns:
         accumulator = numpy array of accumulated values. 
         '''
 
         for i in range(len(x_coords)):
-            #Grab a single point
-            x = x_coords_scaled[i]
-            y = y_coords_scaled[i]
+            # Grab a single point
+            x = x_coords[i]  # TODO This was originally called 'scaled-x'. Verify if a method needs to used to scale
+            y = y_coords[i]  # TODO This was originally called 'scaled-y'. Verify if a method needs to used to scale
 
-            #Actually do transform!
-            curve_prho = x*np.cos(self.thetas)+y*np.sin(self.thetas)
+            # Actually do transform!
+            curve_prho = x * np.cos(self.thetas) + y * np.sin(self.thetas)
 
             for j in range(len(self.thetas)):
-                #Make sure that the part of the curve falls within our accumulator
-                if np.min(abs(curve_rhos[j]-self.rhos)) <= 1.0:
-                    #Find the cell our curve goes through:
-                    rho_index = argmin(abs(curve_rhos[j]-self.rhos))
-                    accumulator[rho_index, j]+=1
+                # Make sure that the part of the curve falls within our accumulator
+                if np.min(abs(curve_prho[j] - self.rhos)) <= 1.0:
+                    # Find the cell our curve goes through:
+                    rho_index = np.argmin(abs(curve_prho[j] - self.rhos))
+                    self.accumulator[rho_index, j] += 1
 
-        return accumulator
-    
+        return self.accumulator
+
     def clear_accumulator(self):
         '''
         Zero out accumulator
         '''
-        self.accumulator = np.zeros((phi_bins, theta_bins))
+        self.accumulator = np.zeros((self.phi_bins, self.theta_bins))
